88380e22e0893d362b9cce044c878ae7cd10bcf4
ShivamKumarr7 <74189499+ShivamKumarr7@users.noreply.github.com>
2021-11-02 14:01:19 +0530

    Add files via upload

---
+++ 02-11-2021.cpp
@@ -0,0 +1,86 @@
+// Q1)  344A  MAGNETS 
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define mod 1000000007
+#define pi 3.1415
+#define mp make_pair
+#define pb push_back
+#define setbits(x) __builtin_popcount(x)
+#define zerobits1(x) __builtin_clz(x)
+#define zerobits2(x) __builtin_ctz(x)
+const int Max = 100005;
+const int BMax = 10000005;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<string>s(n,"0");
+    for(int i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }
+    int ct = 1;
+    for(int i=0;i<n-1;i++)
+    {
+        if(s[i] != s[i+1])
+        {
+            ct++;
+        }
+    }
+    cout<<ct;
+    return 0;
+}
+*/
+
+
+
+// Q2) 1030A    IN SEARCH OF AN EASY PROBLEM
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007
+#define pi 3.1415
+#define setbits(x) __builtin_popcount(x)
+#define zerobits1(x) __builtin_clz(x)
+#define zerobits2(x) __builtin_ctz(x)
+const int Max = 100005;
+const int BMax = 10000005;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    bool check = true;
+    while(n--)
+    {
+        int n;
+        cin>>n;
+        if(n == 1)
+        {
+            check = false;
+        }
+    }
+    if(check == true)
+    {
+        cout<<"EASY";
+    }
+    else 
+    {
+        cout<<"HARD";
+    }
+    return 0;
+}
+*/
+
