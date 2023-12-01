1d62badf7c2b430b2cfef36c1862c099ca71ffd0
ShivamKumarr7 <74189499+ShivamKumarr7@users.noreply.github.com>
2023-12-01 12:18:17 +0530

    Create 1900A.cpp

---
+++ 1900A.cpp
@@ -0,0 +1,88 @@
+/* shivam */
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define setbits(x) __builtin_popcount(x)
+#define zerobits(x) __builtin_ctz(x)
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define MOD 1000000007
+#define all(x) (x).begin(), (x).end()
+ 
+#define debug(x)  cout<<#x<<" : "; _print(x); cout<<"\n";
+ 
+void _print(ll t) {cout << t;}
+void _print(int t) {cout << t;}
+void _print(string t) {cout << t;}
+void _print(char t) {cout << t;}
+void _print(double t) {cout << t;}
+void _print(bool t) {cout << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T> void _print(unordered_set <T> v);
+template <class T,class V> void _print(unordered_map <T,V> v);
+template <class T, class V> void _print(pair <T, V> p) {_print(p.ff); cout<<" "; _print(p.ss);}
+template <class T> void _print(vector <T> v) {for (T i : v) {_print(i); cout << " ";}}
+template <class T> void _print(set <T> v) {for (T i : v) {_print(i); cout << " ";}}
+template <class T> void _print(multiset <T> v) {for (T i : v) {_print(i); cout << " ";}}
+template <class T, class V> void _print(map <T, V> v) {for (auto i : v) {_print(i); cout << " ";}}
+template <class T> void _print(unordered_set <T> v)  {for(T i : v) {_print(i); cout<< " ";}}
+template <class T,class V> void _print(unordered_map <T,V> v) {for (auto i : v) {_print(i.ff); cout<<"->"; _print(i.ss); cout<<"\n";}}
+ 
+ 
+int main()
+{
+    fastio();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        bool check_for_sequence = false;
+        string seq = "...";
+        for(int i=0;i<=n-3;i++)
+        {
+            string temp = "";
+            temp.pb(s[i]);
+            temp.pb(s[i+1]);
+            temp.pb(s[i+2]);
+            if(temp == seq)
+            {
+                check_for_sequence = true;
+                break;
+            }
+        }
+        if(check_for_sequence)
+        {
+            cout<<(2)<<"\n";
+        }
+        else 
+        {
+            int ct = 0;
+            for(int i=0;i<n;i++)
+            {
+                if(s[i] != '#')
+                {
+                    ct += 1;
+                }
+            }
+            cout<<ct<<"\n";
+        }
+    }
+    return 0;
+}
+
